40d5743814386a047e56b8318965a4336b6ae51a
Jonathan Chaney <jonchaney@gmail.com>
2017-06-12 13:41:54 -0700

    poker

--- W2D3/Poker/lib/game.rb
+++ W2D3/Poker/lib/game.rb
@@ -11,42 +11,42 @@ class Game
   end
 
   def play_game
-    # hand1 = players.first.draw(deck)
-    # hand2 = players.last.draw(deck)
-    #
-    # p hand1.card_info
-    # p hand2.card_info
-    #
-    # case hand1 <=> hand2
-    # when -1
-    #   puts "#{players.last.name} has won with #{hand2.type}!"
-    #   puts "#{players.first.name} has lost with #{hand1.type}!"
-    #   players.first.pot -= 5
-    #   players.last.pot += 5
-    # when 1
-    #   puts "#{players.first.name} has won with #{hand1.type}!"
-    #   puts "#{players.last.name} has lost with #{hand2.type}!"
-    #   players.last.pot -= 5
-    #   players.first.pot += 5
-    # else
-    #   puts "it's a tie!"
-    # end
-    #
-    # puts "#{players.first.name} has #{players.first.pot}"
-    # puts "#{players.last.name} has #{players.last.pot}"
+    hand1 = players.first.draw(deck)
+    hand2 = players.last.draw(deck)
+
+    p hand1.card_info
+    p hand2.card_info
+
+    case hand1 <=> hand2
+    when -1
+      puts "#{players.last.name} has won with #{hand2.type}!"
+      puts "#{players.first.name} has lost with #{hand1.type}!"
+      players.first.pot -= 5
+      players.last.pot += 5
+    when 1
+      puts "#{players.first.name} has won with #{hand1.type}!"
+      puts "#{players.last.name} has lost with #{hand2.type}!"
+      players.last.pot -= 5
+      players.first.pot += 5
+    else
+      puts "it's a tie!"
+    end
+
+    puts "#{players.first.name} has #{players.first.pot}"
+    puts "#{players.last.name} has #{players.last.pot}"
 
   end
 
 end
+deck = Deck.new
+player1 = Player.new("jon", 100, deck)
+player2 = Player.new("li hsuan", 100, deck)
 
-# player1 = Player.new("jon", 100)
-# player2 = Player.new("li hsuan", 100)
-#
-# user_desire = "y"
-#
-# while user_desire == "y"
-#   game = Game.new(player1, player2)
-#   game.play_game
-#   puts "Do you want to continue?"
-#   user_desire = gets.chomp
-# end
+user_desire = "y"
+
+while user_desire == "y"
+  game = Game.new(player1, player2)
+  game.play_game
+  puts "Do you want to continue?"
+  user_desire = gets.chomp
+end
